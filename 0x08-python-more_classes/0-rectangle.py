6dd530284ccb2fb36b053e496914b305c15cb921
kevinmid3 <kevinmid3@gmail.com>
2023-04-05 21:59:57 -0700

    Task

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+This module is composed by a function that adds two numbers
+in more classes
+"""
+class Rectangle:
+    """ Void class """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,3 @@
+# More Classes and Objects
+
+>
